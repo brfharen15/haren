531bbe9b7c7ea1d18eb671bfeebc745ba9d52b48
Johan Andersson <johan.rejeep@gmail.com>
2014-10-15 08:50:04 +0200

    Add Refinery CMS project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Haren::Application.config.secret_token = '0a56b366c08a59e718c9a347f51d60dca0bb4552ffeec45f7b8bbe097a012434e637c041bd22ea037fa0b558dfe081083e1e763d49039211285212fd817a1fce'
